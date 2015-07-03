4f519b472ac6cf59fb02cf27467ef6b1e5426300
root <root@raspberrypi.(none)>
2015-07-03 13:56:40 +0100

    Add initial install scripts

---
+++ install.fs
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo -n "Copying files to system locations..."
+
+cp ./fs/etc/* /etc/ -rf
+cp ./fs/sbin/* /sbin/ -rf
+cp ./fs/opt/* /opt/ -rf
+
+echo "Done"
+
+echo -n "Configure DHCP/DNS..."
+rm -rf /var/lib/dhcp
+ln -s /tmp /var/lib/dhcp
+
+rm /etc/resolv.conf
+ln -s /tmp/resolv.conf /etc/resolv.conf
+
+echo "Done"
+
+echo -n "Linking rc scripts..."
+ln -s /opt/bin/shutdown /etc/init.d/shutdown
+insserv -d shutdown
+echo "Done"
+
